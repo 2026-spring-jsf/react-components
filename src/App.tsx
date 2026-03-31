07336d60db5c7e01308003871fca1b137dd77738
tsteelematc <tsteele@madisoncollege.edu>
2026-03-31 14:43:46 -0500

    Import the stylesheet and change fragment to div with margin...

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div
+      className='m-3'
+    >
       <h1>
         React Components
       </h1>
-    </>
+    </div>
   )
 }
 
