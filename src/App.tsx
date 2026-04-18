af756cd7991b208d8066b60c2238d6e2636c2495
Michael Rooney <msroone@gmail.com>
2026-04-18 17:32:52 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MrooneyButton from './mrooneyButton';
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import PizzaToppings from "./PizzaToppings";
 import TsteeleComponent from "./TsteeleComponent";
 import Welcome from "./Welcome";
 import ThamiltonComponent from "./ThamiltonComponent";
+import MrooneyButton from './mrooneyButton';
 
 const App = () => {
   return (
     <div className="m-3">
       <h1>React Components</h1>
       <PizzaToppings />
-      <MrooneyButton />
       <TsteeleComponent />
       <Welcome />
       <ThamiltonComponent />
+      <MrooneyButton />
     </div>
   );
 };
